4b6b1ae6aadb772e8510afa2020af2a275c16247
simonshy <64548770+simonshy@users.noreply.github.com>
2022-06-26 23:43:16 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	$(".menclose, .meniu").click(function(){
+		$(".menclose").toggleClass("deschide");
+    $(".meniu").toggleClass("inchide");
+	});
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("intro"),
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  path:"introSS.json"
+});
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("talpa"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path:"SStalpa.json"
+});
